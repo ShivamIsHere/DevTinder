8b54ce80b5dbea285c8654d9ece7d92fa7fd2dc4
ShivamIsHere <singhkumarshivam15@gmail.com>
2024-12-22 16:31:05 +0530

    created middleware and route handler

--- src/app.js
+++ src/app.js
@@ -2,14 +2,29 @@ const express = require('express');
 
 const app=express();
 
+const {adminAuth,userAuth}=require("./middlewares/auth");
 
-app.get("/",(req,res)=>{
-    res.send("This is Home Page")
+app.use("/admin",adminAuth);
+app.get("/user/login",(req,res)=>{
+    res.send("user is logged in")
 });
-app.get("/profile",(req,res)=>{
+app.get("/user",userAuth,(req,res)=>{
+    res.send("User is Authorized")
+})
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("All Data Sent");
+})
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("Deleted a user");
+})
+app.use("/",(req,res,next)=>{
+    // res.send("This is Home Page")
+    next();
+});
+app.use("/profile",(req,res)=>{
     res.send("This is Profile Page")
 });
-app.get("/test",(req,res)=>{
+app.use("/test",(req,res)=>{
     res.send("Hello Hello Hello")
 });
 
